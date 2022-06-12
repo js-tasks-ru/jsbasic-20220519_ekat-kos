0fe6f06dbe974b17af9e840d8f8cd2a228315e28
Ekaterina Kostina <ekat.kos@email.cz>
2022-06-12 10:00:00 +0200

    module-5 tasks 1,2,3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+
+  const toggleBtn = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+
+  toggleBtn.addEventListener("click", () => {
+
+    text.hidden = !text.hidden;
+
+  }
+  );
 }
