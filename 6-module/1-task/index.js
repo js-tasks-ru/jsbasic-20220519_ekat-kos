57fb081d4f4d5ef533bb1bc09331fcc38155153a
Ekaterina Kostina <ekat.kos@email.cz>
2022-06-15 12:46:29 +0200

    add module-6 task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,56 @@
  *
  */
 export default class UserTable {
+  #elem = [];
+  #rows = [];
+
   constructor(rows) {
+    this.#elem = document.createElement("table");
+    this.#rows = rows;
+    this.render();
+  }
+
+  get elem() {
+    return this.#elem;
+  }
+
+  get container() {
+    return this.#elem;
+  }
+
+  render() {
+
+    this.#elem.innerHTML = this.#template();
+    this.container.addEventListener("click", this.#removeBtn);
+  }
+
+  #removeBtn = (event) => {
+
+    if (event.target.dataset.action != "remove-button") return;
+    let td = event.target.closest("tr");
+    td.remove();
+
+  }
+
+  #template() {
+    return `
+    <thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>` + this.#rows.map(item => `
+  <tr>
+      <td>${item.name}</td>
+      <td>${item.age}</td>
+      <td>${item.salary}</td>
+      <td>${item.city}</td>
+      <td><button data-action="remove-button">X</button></td>
+  </tr>`)
+        .join("") + `</tbody>`;
+
   }
 }
