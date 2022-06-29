3a366bf3060ba421abe5fadea2a87c320ea23243
Ekaterina Kostina <ekat.kos@email.cz>
2022-06-29 13:54:10 +0200

    fix modules

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,7 +2,6 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
-
   constructor(products) {
     this.products = products;
     this.filters = {};
